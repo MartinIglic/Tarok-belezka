e5922ee36c25f1a353729c8df89f88fa6c20f75f
Martin Iglič <martin.iglic@gmail.com>
2020-09-22 16:14:28 +0200

    začetek komitov

--- model_beležke.py
+++ model_beležke.py
@@ -1,4 +1,7 @@
 slovar_iger = {'tri': 10, 'dve': 20, 'ena': 30, 'solo tri': 40, 'solo dve': 50, 'solo ena': 60, 'pikolo berač': 60, 'berač': 70, 'solo brez': 80, 'klop': 0, 'bar.valat_tri': 100, 'bar.valat_dve': 110, 'bar.valat_ena': 120, 'bar.valat_solo-tri': 125, 'bar.valat_solo-dve': 150, 'bar.valat_solo-ena': 175, 'bar.valat_solo-brez': 250, 'valat_tri':200, 'valat_dve':220, 'valat_ena':240, 'valat_solo-tri':250, 'valat_solo-dve':300, 'valat_solo-ena':350, 'valat_solo_brez':500}
 slovar_bonusov = {'kralji': 10, 'trula': 10, 'pagat ultimo': 25,'kralj ultimo': 10, 'valat': 50}
 
-#tra kakkaka              
+class Miza:
+
+    def tralalal(self):
+        pass
