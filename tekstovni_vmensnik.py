2455fb62276736391cbcbd755ec7f6c0036cd464
Martin Iglič <martin.iglic@gmail.com>
2020-10-05 21:19:10 +0200

    dodajanje funkcij v tekstovni vmesnik

--- tekstovni_vmensnik.py
+++ tekstovni_vmensnik.py
@@ -1,10 +1,54 @@
-from model_beležke import Miza
+from model import Miza
+
+prvaMiza = Miza(ime_mize='Kvartopirci', ime_1='Taroker', ime_2='Pagater', ime_3='Mondfanger', ime_4='Špilfarderber')
+
+def dodaj_novo_mizo():
+    print('Uspešno ste dodali novo mizo')
+
+def posodobi_podatke():
+    print('Kako se imenuje vaša miza?')
+    input('> ')
+    
+    pass
+
+def poglej_dosedanje_rezultate():
+    pass
+
+def izhod():
+    pass
+
+
+
+def glavni_meni():
+    print('Kaj želite storiti?')
+    print('1) Dodaj novo mizo')
+    print('2) Posodobi podatke')
+    print('3) Poglej dosedanje rezultate ')
+    print('4) Izhod')
+    izbira = input('> ')
+    if izbira == '1':
+        dodaj_novo_mizo()
+    elif izbira == '2':
+        posodobi_podatke()
+    elif izbira == '3':
+        poglej_dosedanje_rezultate()
+    elif izbira == '4':
+        izhod()
+    else:
+        print ('Prosim izberite eno izmed podanih možnosti')
+
+
+
 
 
 
 def main():
+    print('Pozdravljeni v programu Tarok beležka.')
+
+    while True:
+        
+        glavni_meni()
     
-    pozdrav()
 
 
 
