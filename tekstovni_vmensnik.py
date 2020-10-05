f3576d5dca1991a6426819cf9c93a10121cd8c14
Martin Iglič <martin.iglic@gmail.com>
2020-10-05 23:07:22 +0200

    dodal funkcijo dodaj_novo_mizo in deluje

--- tekstovni_vmensnik.py
+++ tekstovni_vmensnik.py
@@ -1,9 +1,28 @@
 from model import Miza
+from model import preveriAliObstajaMiza
+from model import preglejImenaIgralcev
+from model import dodajMizo
 
 prvaMiza = Miza(ime_mize='Kvartopirci', ime_1='Taroker', ime_2='Pagater', ime_3='Mondfanger', ime_4='Špilfarderber')
 
 def dodaj_novo_mizo():
-    print('Uspešno ste dodali novo mizo')
+    print('Kako želite poimenovati svojo mizo?')
+    ime_mize = input('> ')
+    if preveriAliObstajaMiza(ime_mize) == True:
+        print(f'Žal miza z imenom {ime_mize} že obstaja. Prosim, poskusite drugo ime. ')
+    else:
+        print(f'Ime mize je sprejemnljivo. Vaša miza se bo imenovala {ime_mize}.')
+        print('Kako je ime igralcem? Prosim Vas, da se imena igralcev med seboj razlikujejo za vsaj en znak.')
+        ime_igralca_1 = input('> Prvemu igralcu bo ime...')
+        ime_igralca_2 = input('> Drugemu igralcu bo ime...')
+        ime_igralca_3 = input('> Tretjemu igralcu bo ime...')
+        ime_igralca_4 = input('> Četrtemu igralcu bo ime...')
+        if preglejImenaIgralcev(ime_igralca_1, ime_igralca_2, ime_igralca_3, ime_igralca_4) == True:
+            print(f'Imena so sprejemljiva. Vaša miza{ime_mize} ima igralce {ime_igralca_1}, {ime_igralca_2}, {ime_igralca_3}, {ime_igralca_4}.')
+            dodajMizo(ime_mize, ime_igralca_1, ime_igralca_2, ime_igralca_3, ime_igralca_4)
+            print('Uspešno ste dodali novo mizo')
+        else:
+            print('Imena se ponavljajo. Prosim izberite druga imena.')
 
 def posodobi_podatke():
     print('Kako se imenuje vaša miza?')
