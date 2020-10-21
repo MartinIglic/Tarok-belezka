674f81ec3d659a53828a4bd23381490f66f16542
Martin Iglič <martin.iglic@gmail.com>
2020-10-21 15:35:31 +0200

    nekaj testiranj

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
-slovar_iger = {'tri': 10, 'dve': 20, 'ena': 30, 'solo tri': 40, 'solo dve': 50, 'solo ena': 60, 'pikolo berač': 60, 'berač': 70, 'solo brez': 80, 'klop': 0, 'bar.valat_tri': 100, 'bar.valat_dve': 110, 'bar.valat_ena': 120, 'bar.valat_solo-tri': 125, 'bar.valat_solo-dve': 150, 'bar.valat_solo-ena': 175, 'bar.valat_solo-brez': 250, 'valat_tri':200, 'valat_dve':220, 'valat_ena':240, 'valat_solo-tri':250, 'valat_solo-dve':300, 'valat_solo-ena':350, 'valat_solo_brez':500}
+slovar_iger = {'tri': 10, 'dve': 20, 'ena': 30, 'solo tri': 40, 'solo dve': 50, 'solo ena': 60, 'solo brez': 80, 'pikolo berač': 60, 'berač': 70}
+barvni_valati = {'barvni valat tri': 100, 'barvni valat dve': 110, 'barvni valat ena': 120, 'barvni valat solotri': 125, 'barvni valat solo dve': 150, 'barvni valat solo ena': 175, 'bar valat solo brez': 250}
+valat = {'valat tri':200, 'valat dve':220, 'valat ena':240, 'valat solo tri':250, 'valat solo dve':300, 'valat solo ena':350, 'valat solo brez':500}
 slovar_bonusov = {'kralji': 10, 'trula': 10, 'pagat ultimo': 25,'kralj ultimo': 10, 'valat': 50}
 import json
 
@@ -10,6 +12,16 @@ class Igralec:
         self.tocke = tocke
         self.radelci = radelci
         self.ime = ime
+
+    def posodobi_tocke(self, tocke, mat):
+        if self.porabiRadelc(mat) == True:
+            self.dodaj_tocke(tocke*2)
+            print(f'Igralec {self.ime} je prejel {tocke*2} točk.')
+        else:
+            self.dodaj_tocke(tocke)
+            print(f'Igralec {self.ime} je prejel {tocke} točk.')
+
+        
 
 
     def dodaj_tocke(self, tocke):
@@ -27,11 +39,15 @@ class Igralec:
         else: 
             return False
 
-    def porabiRadelc(self):
-        if preveriRadelc() is False:
-            self.radelci -= 1
+    def porabiRadelc(self, mat):
+        if self.preveriRadelc() is False:
+            if mat == 1:
+                self.radelci -= 1
+                print(f'Igralec {self.ime} je porabil en radelc')
+            return True
         else:
-            pass
+            return False
+            
 
     def podatkiOIgralcu(self):
         slovar_podatkov = {}
@@ -98,7 +114,11 @@ class Belezka:
 #            slovar_stanja = json.load(datoteka)
 #        posodobljena_miza = ustvariMizo(slovar_stanja)
 #        return Belezka()
-            
+
+
+def dodajMizo( ime_mize, ime_igralca_1, ime_igralca_2, ime_igralca_3, ime_igralca_4):
+    miza = Miza(ime_mize, ime_igralca_1, ime_igralca_2, ime_igralca_3, ime_igralca_4)
+    objekti_Miza[miza.ime] = miza          
 
 def slovarIgralca(slovar):
     ime_igralca = slovar['ime igralca']
@@ -108,12 +128,12 @@ def slovarIgralca(slovar):
 
 
 def vrniSteviloRadelcev(ime_mize, ime_igralca):
-    miza = objekti_Miza[poisciMizo(ime_mize)]
+    miza = poisciMizo(ime_mize)
     igralec = miza.poisciIgralca(ime_igralca)
     return igralec.radelci
 
 def vrniSteviloTock(ime_mize, ime_igralca):
-    miza = objekti_Miza[poisciMizo(ime_mize)]
+    miza = poisciMizo(ime_mize)
     igralec = miza.poisciIgralca(ime_igralca)
     return igralec.tocke
 
@@ -126,15 +146,65 @@ def bonusTočke(bonus):
 
 def seznamImenMiz():
     mnozica = set()
-    seznam = objekti_Miza
-    for miza in seznam:
-        mnozica.add(miza.ime_mize)
+    slovar = objekti_Miza
+    for miza in slovar:
+        iskana = objekti_Miza.get(miza)
+        mnozica.add(iskana.ime)
     return mnozica
     
 
 def preveriAliObstajaMiza(ime_mize):
         mnozica = seznamImenMiz()
         return ime_mize in mnozica
+
+def vrziVkup(igra, slovarIger, razlika, napovedi, realizacije, mat):
+    seznam = seznam_tock(slovarIger)
+    točkeIgra = seznam[igra - 1] * mat
+    točkeIgra = točkeIgra + razlika
+    bonusi = racunajBonuse(napovedi, realizacije)
+    tocke = točkeIgra + bonusi
+    return tocke
+
+
+def posodobi_radelc(ime_mize, ime_igralca, slovarIger, igra):
+    if igra > 6 or slovarIger is not slovar_iger:
+        miza = poisciMizo(ime_mize)
+        miza.igralec_1.radelci += 1
+        miza.igralec_2.radelci += 1
+        miza.igralec_3.radelci += 1
+        miza.igralec_4.radelci += 1
+        print(f'Vsi igralci mize {ime_mize} so prejeli radelc.')
+    else:
+        pass
+
+
+def racunajBonuse(napovedi, realizacije):
+    tocke = 0
+    i = 0
+    r = 1
+    for napoved in napovedi:
+        if napoved == 1 and realizacije[i] == 1:
+            r = 2
+        elif napoved == 1 and realizacije[i] == 0:
+            r = -2
+        elif napoved == 0 and realizacije[i] == 1:
+            r = 1
+        else :
+            r = 0    
+        tocke += seznam_tock(slovar_bonusov)[i] * r 
+        i += 1
+    return tocke
+
+def dodaj_tocke_igri(ime_igralec, ime_soigralec, ime_miza, tocke, mat):
+    miza = objekti_Miza[ime_miza]
+    igralec = miza.poisciIgralca(ime_igralec)
+    soigralc = miza.poisciIgralca(ime_soigralec)
+    if soigralc == None:
+        pass
+    else:
+        soigralc.dodaj_tocke(tocke)
+        print(f'Soigralec {ime_soigralec} je prejel {tocke} točk.')
+    igralec.posodobi_tocke(tocke, mat)
     
         
 def preveri_ali_obstaja_igralec(ime_mize, ime_igralca):
@@ -151,20 +221,15 @@ def preveri_ali_obstaja_igralec(ime_mize, ime_igralca):
             print(f'Miza z imenom {ime_mize} še ne obstaja.')
 
 def poisciMizo(ime_mize):
-    if not ime_mize in seznamImenMiz:
-        print('miza s tem imenon ne obstaja!!!')
-    else:
-        i = 0
-        for miza in objekti_Miza:
-            if miza[i].ime_mize == ime_mize:
-                return i
-            else:
-                i += 1
+#    if not ime_mize in seznamImenMiz:
+#        print('miza s tem imenon ne obstaja!!!')
+#    else:
+    return objekti_Miza.get(ime_mize)
         
 
 def seznamIgralcev(ime_mize):
     if preveriAliObstajaMiza(ime_mize) == True:
-        miza = objekti_Miza[ime_mize]
+        miza = objekti_Miza.get(ime_mize)
         ime_1 = miza.igralec_1.ime
         ime_2 = miza.igralec_2.ime
         ime_3 = miza.igralec_3.ime
@@ -174,6 +239,8 @@ def seznamIgralcev(ime_mize):
         print(f'Miza z imenom {ime_mize} še ne obstaja.' )
 
 def preglejImenaIgralcev(ime_1, ime_2, ime_3, ime_4):
+    # PReverimo, da so imena res različna
+    # Če niso različna ne moremo vedeti kdo igra
     seznam_igralcev = [ime_1, ime_2, ime_3, ime_4]
     množica_imen = set(seznam_igralcev)
     if len(množica_imen) < 4:
@@ -191,7 +258,7 @@ def ustvariMizo(slovar):
     igralec_4 = podatki['igralec 4']
     miza = Miza(ime_mize, igralec_1['ime igralca'], igralec_2['ime igralca'], igralec_3['ime igralca'], igralec_4['ime igralca'])
     miza.posodobiTočkeRadelce(igralec_1, igralec_2, igralec_3, igralec_4)
-    objekti_Miza.append(miza)
+    objekti_Miza[miza.ime] = miza
      
 def naloziMizo(ime_datoteke):
         with open(ime_datoteke) as datoteka:
@@ -200,27 +267,42 @@ def naloziMizo(ime_datoteke):
         return posodobljena
 
 def nalozi_stanje(ime_datoteke):
-        with open(ime_datoteke) as datoteka:
+        with open(ime_datoteke, encoding='UTF-8') as datoteka:
             slovar_stanja = json.load(datoteka)
         for miza in slovar_stanja:
             slovarMize = slovar_stanja[miza]
             ustvariMizo(slovarMize)
+
+def naredi_seznam(napoved):
+    sez = list(napoved)
+    nabor = [0,0,0,0,0]
+    for st in sez:
+        stevilo = int(st) - 1
+        nabor[stevilo] = 1
+    stevke = tuple(nabor)
+    return stevke
         
+
+def seznam_tock(slovar):
+    return list(slovar.values())
 
 def zapisiMize(ime_datoteke):
         for mizica in objekti_Miza:
-            slovar = mizica.podatkiOMizi()
-            slovar_za_JSON[mizica.ime] = slovar
+            slovar = objekti_Miza.get(mizica)
+            zapisi = slovar.podatkiOMizi()
+            slovar_za_JSON[mizica] = zapisi
         
-        with open(ime_datoteke, 'w') as datoteka:
+        with open(ime_datoteke, 'w', encoding='UTF-8') as datoteka:
             json.dump(slovar_za_JSON, datoteka, ensure_ascii=False, indent=4)        
   
 
-objekti_Miza = []
+objekti_Miza = dict()
+
+nalozi_stanje('belezka.json') #  {'cjzvhm': Miza('cjzvhm', 'cjvukzgbuli', 'vkgvjbh', 'bjvhbjl', 'bjh,b j,'), }
 
 slovar_za_JSON = {}
 
-nalozi_stanje('belezka.json')
+#nalozi_stanje('belezka.json')
 
 slovarJSON = {'ime mize': 'druga', 'igralec 1': {'ime igralca': 'tilen', 'točke igralca': 50, 'radelci igralca': 0}, 'igralec 2': {'ime igralca': 'klemen', 'točke igralca': 0, 'radelci igralca': 0}, 'igralec 3': {'ime igralca': 'jani', 'točke igralca': 0, 'radelci igralca': 0}, 'igralec 4': {'ime igralca': 'bojan', 'točke igralca': 0, 'radelci igralca': 0}}
 
@@ -228,10 +310,18 @@ NoviJohny = {'ime mize': 'deseta', 'igralec 1': {'ime igralca': 'miha', 'točke 
 
 seNovejsiJohny = {'ime mize': 'stota', 'igralec 1': {'ime igralca': 'bine', 'točke igralca': 50, 'radelci igralca': 0}, 'igralec 2': {'ime igralca': 'sara', 'točke igralca': 0, 'radelci igralca': 0}, 'igralec 3': {'ime igralca': 'tara', 'točke igralca': 0, 'radelci igralca': 0}, 'igralec 4': {'ime igralca': 'ema', 'točke igralca': 0, 'radelci igralca': 0}}
 
-
 #ustvariMizo(NoviJohny)
 
 #ustvariMizo(seNovejsiJohny)
+
+preglejImenaIgralcev
+
+
+#Vse_trenutne_mize = seznamImenMiz()
+
+#ALIjevdesetimiha = preveri_ali_obstaja_igralec('deseta', 'miha')
+
+#radelcienga = vrniSteviloRadelcev('stota', 'bine')
 
 #poskus = ustvariMizo(slovarJSON)
 
--- tekstovni_vmensnik.py
+++ tekstovni_vmensnik.py
@@ -1,7 +1,19 @@
-from model import Miza
-from model import preveriAliObstajaMiza
-from model import preglejImenaIgralcev
-from model import dodajMizo
+from model import Miza, preveriAliObstajaMiza, preglejImenaIgralcev, valat
+from model import dodajMizo, objekti_Miza, slovar_za_JSON, seznamIgralcev
+from model import slovar_bonusov, slovar_iger, barvni_valati, nalozi_stanje
+from model import Igralec, vrziVkup, dodaj_tocke_igri, zapisiMize
+from model import naredi_seznam, posodobi_radelc
+
+navadneIgre = '''1)tri 
+2)dve 
+3)ena 
+4)solo tri 
+5)solo dve 
+6)solo ena 
+7)solo brez 
+8)pikolo berač 
+9)berač'''
+seznam_bonusov = ['kralji', 'trula', 'pagat ultimo', 'kralj ultimo', 'valat']
 
 prvaMiza = Miza(ime_mize='Kvartopirci', ime_1='Taroker', ime_2='Pagater', ime_3='Mondfanger', ime_4='Špilfarderber')
 
@@ -18,76 +30,228 @@ def dodaj_novo_mizo():
         ime_igralca_3 = input('> Tretjemu igralcu bo ime...')
         ime_igralca_4 = input('> Četrtemu igralcu bo ime...')
         if preglejImenaIgralcev(ime_igralca_1, ime_igralca_2, ime_igralca_3, ime_igralca_4) == True:
-            print(f'Imena so sprejemljiva. Vaša miza{ime_mize} ima igralce {ime_igralca_1}, {ime_igralca_2}, {ime_igralca_3}, {ime_igralca_4}.')
+            print(f'Imena so sprejemljiva. Vaša miza {ime_mize} ima igralce {ime_igralca_1}, {ime_igralca_2}, {ime_igralca_3}, {ime_igralca_4}.')
             dodajMizo(ime_mize, ime_igralca_1, ime_igralca_2, ime_igralca_3, ime_igralca_4)
-            Miza(ime_mize,ime_igralca_1,ime_igralca_2,ime_igralca_3,ime_igralca_4)
             print('Uspešno ste dodali novo mizo')
         else:
             print('Imena se ponavljajo. Prosim izberite druga imena.')
 
 def posodobi_podatke():
-    print('Kako se imenuje vaša miza?')
-    ime_mize = input('> ')
-    if preveriAliObstajaMiza(ime_mize) == True:
-        print('Za mizo')
+    while True:
+        print('Kako se imenuje vaša miza?')
+        ime_mize = input('> ')
+        if preveriAliObstajaMiza(ime_mize) == True:
+            print(f'Miza {ime_mize} sprejeta.')
+            print('Ali ste igrali klopa?')
+            print('1) Ja')
+            print('2) Ne')
+            odgovor = input('>')
+            if odgovor == '1':
+                zberiRezultateKlopa(ime_mize)
+                break
+            elif odgovor == '2':
+                print('Kateri igralec je šel igrati?')
+                seznam = ponudiIgralce(ime_mize)
+                stevnik = input('> ') 
+                st = int(stevnik)
+                ime_igralca = seznam[st - 1]
+                igra = licitiranaIgra(ime_mize, ime_igralca)
+                soigralc = soigralec(igra, ime_igralca, ime_mize)
+                slovarIger = mordaValat(igra)
+                raz = razlikaIgre(slovarIger, igra)
+                mat = dobljena()
+                napoved = seznamDodatkov(slovarIger, igra, 'napovedi')
+                real = seznamDodatkov(slovarIger, igra, 'realizacije')
+                tocke = vrziVkup(igra, slovarIger, raz, napoved, real, mat)
+                dodaj_tocke_igri(ime_igralca, soigralc, ime_mize, tocke, mat)
+                posodobi_radelc(ime_mize, ime_igralca, slovarIger, igra)
+                print('Uspešno ste dodali točke.')
+                break
+            else:
+                print('Prosim, pritisnite število 1 ali 2.')
+        else:
+            print('Miza s tem imenom ne obstaja. \nPoskusite drugo ime!')
+        
+       
+        
+
+def licitiranaIgra(ime, polnaRit):
+    miza = objekti_Miza[ime]
+    print(f'Kaj je šel {polnaRit} igrati?')
+    return licitirajObično()
+   
+
+def licitirajObično():
+    print(navadneIgre)
+    indeks = input('> ')
+    return  int(indeks)
+
+
+def soigralec(igra, ime, ime_mize):
+    if igra < 4:
+        print('Ali ste imeli soigralca?')
+        print('1) Ja')
+        print('2) Ne')
+        odgovor = input('> ')
+        if odgovor == '1':
+            print('Koga ste klicali?')
+            igralci = seznamIgralcev(ime_mize)
+            ponudiIgralce(ime_mize)
+            indeks = input('> ')
+            ind = int(indeks)
+            return igralci[ind - 1]
+    else:
+        return None
+        
+
+def mordaValat(igra):
+    if igra <= 7:
+        print('Ali ste igrali barvnega valata?')
+        print('1) Ja')
+        print('2) Ne')
+        odgovor = input('> ')
+        if odgovor == '1':
+            return barvni_valati
+        else:
+            print('Ali ste igrali šli igrat navadnega valata?')
+            print('1) Ja')
+            print('2) Ne')
+            morda = input('> ')
+            if morda == '1':
+                return valat
+            else:
+                return slovar_iger
+    else:
+        return slovar_iger
+
+def razlikaIgre(slovarIger, igra):
+    tocke = 0 
+    if slovarIger == slovar_iger and igra <= 6:
+        while True:
+            print('Koliko je bilo razlike?')
+            razlika = input('> ')
+            raz = int(razlika)
+            if raz < 35:
+                tocke += raz
+                return tocke
+            else:
+                print('Vpisali ste preveliko razliko.')
+                print('Razlika mora biti manjša od 35.')
+    return tocke
+            
+def seznamDodatkov(slovarIger, igra, vrsta):
+    seznam = []
+    if slovarIger == slovar_iger and igra <= 6:
+        print(f'Ali ste imeli {vrsta}?')
+        print('1) Ja')
+        print('2) Ne')
+        morda = input('> ')
+        if morda == '1':
+            print(f'Napišite številke {vrsta}.')
+            i = 1
+            for napoved in seznam_bonusov:
+                print(f'{i}){napoved}')
+                i += 1
+            napoved = input('> ')
+            seznam = naredi_seznam(napoved)
+            return seznam
+        else:
+            return (0,0,0,0,0)
+    else:
+        return (0,0,0,0,0)
+
+
+def dobljena():
+    print('Ali ste igro dobili ali izgubili?')
+    print('1) Dobili.')
+    print('2) Izgubili')
+    while True:
+        rez = input('> ')
+        if rez == '1':
+            return 1
+        elif rez == '2':
+            return -1
+        else:
+            print('Prosim vnesiti število ali 1 ali 2.')
+
+
+
+def zberiRezultateKlopa(ime):
+    miza = objekti_Miza[ime]
+    seznam = seznamIgralcev(ime)
+    for ime_igralec in seznam:
+        igralec = miza.poisciIgralca(ime_igralec)
+        print(f'Koliko točk je zbrala oseba {ime_igralec}?')
+        tocke = input('> ')
+        igralec.dodaj_tocke(-(int(tocke)))
+        igralec.dodaj_radelc()
+        print(f'Oseba {ime_igralec} je prejela {int(tocke)} točk in en radelc.')
+        print(f'{ime_igralec} ima {igralec.radelci} radelcev')
+    print('Klop uspešno vnesen')
+
+
+def ponudiIgralce(ime):
+    miza = objekti_Miza[ime]
+    igralci = seznamIgralcev(ime)
+    i = 1
+    for igralec in igralci:
+        print(f'{i}) {igralec}')
+        i += 1
+    return igralci
     
-    pass
 
-def vnosIgralcev():
-    ime_igralca_1 = input('> Prvemu igralcu bo ime...')
-    ime_igralca_2 = input('> Drugemu igralcu bo ime...')
-    ime_igralca_3 = input('> Tretjemu igralcu bo ime...')
-    ime_igralca_4 = input('> Četrtemu igralcu bo ime...')
-
-def izbiranjeMize():
-    print('Kako želite poimenovati svojo mizo?')
-    ime_mize = input('> ')
 
 def poglej_dosedanje_rezultate():
     print('Kako se imenuje vaša miza')
-    ime_mize = input('> ')
-    if preveriAliObstajaMiza(ime_mize) == True:
-        miza = mize[ime_mize]
-        
-def klicanjeMize
-
-def izhod():
-    pass
-
+    while True:
+        ime_mize = input('> ')
+        if preveriAliObstajaMiza(ime_mize) == True:
+            miza = objekti_Miza[ime_mize]
+            seznam = seznamIgralcev(ime_mize)
+            for ime in seznam:
+                igralec = miza.poisciIgralca(ime)
+                rad = igralec.radelci
+                toc = igralec.tocke
+                print(f'Oseba {ime} ima {toc} točk in {rad} neporabljenih radelcev.')
+            print('Uspešno igro želim.')
+            break
+        else:
+            print(f'Miza z imenom{ime_mize} še ne obstaja.')
 
 
 def glavni_meni():
+    while 1:
+        zapisiMize('belezka.json')
+        nalozi_stanje('belezka.json')
+        printajGlavniMeni()
+        izbira = input('>')
+        if izbira == '1':
+            dodaj_novo_mizo()
+        elif izbira == '2':
+            posodobi_podatke()
+        elif izbira == '3':
+            poglej_dosedanje_rezultate()
+        elif izbira == '4':
+            break
+        else:
+            print ('Prosim izberite eno izmed podanih možnosti')
+
+def printajGlavniMeni():
     print('Kaj želite storiti?')
     print('1) Dodaj novo mizo')
     print('2) Posodobi podatke')
     print('3) Poglej dosedanje rezultate ')
     print('4) Izhod')
-    izbira = input('> ')
-    if izbira == '1':
-        dodaj_novo_mizo()
-    elif izbira == '2':
-        posodobi_podatke()
-    elif izbira == '3':
-        poglej_dosedanje_rezultate()
-    elif izbira == '4':
-        izhod()
-    else:
-        print ('Prosim izberite eno izmed podanih možnosti')
-
-
 
 
 
 
 def main():
     print('Pozdravljeni v programu Tarok beležka.')
-
-    while True:
-        
+    nalozi_stanje('belezka.json')
+    while True:        
         glavni_meni()
-    
-
-
-
+        zapisiMize('belezka.json')
+        
 
 main()
